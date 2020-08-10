9fe774a7b16ce08fb19052d3d73ac48640ee4115
cainiaodjm <cainiaodjm@163.com>
2020-08-10 10:22:19 +0800

    first commit

---
+++ 1.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-10 10:20:20
+ * @LastEditTime: 2020-08-10 10:20:36
+ * @LastEditors: your name
+ * @Description: 
+ * @FilePath: /git-exercise/1.js
+ * @可以输入预定的版权声明、个性签名、空行等
+ */
+console.log('Hello World');
